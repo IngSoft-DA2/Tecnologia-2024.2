c85553e55ca11044c8c8c19e0edbfa3b5f79b775
Max Halty <halty.maximo@principal.com>
2024-09-15 20:17:12 -0300

    added missing projects

---
+++ testing-Shop/shop.BusinessLogic/UserService.cs
@@ -0,0 +1,44 @@
+﻿using shop.IBusinessLogic;
+using shop.Domain;
+
+namespace shop.BusinessLogic;
+
+public class UserService : IUserService
+{
+    public UserService() { }
+
+    public User CreateUser(string email, string address, string password)
+    {
+        return new User
+        {
+            Mail = email,
+            Address = address,
+            Password = password
+        };
+    }
+
+    public void DeleteUser(int userId, Guid adminToken)
+    {
+        if (userId <= 0)
+        {
+            throw new ResourceNotFoundException("Such user does not exist.");
+        }
+
+        // Lógica para eliminar al usuario...
+    }
+
+    public bool ValidateUser(User user)
+    {
+        return user.Mail.Contains("@");
+    }
+}
+
+public class ResourceNotFoundException : Exception
+{
+    public ResourceNotFoundException(string message) : base(message) { }
+}
+
+public class ResourceAlreadyExistsException : Exception
+{
+    public ResourceAlreadyExistsException(string message) : base(message) { }
+}
---
+++ testing-Shop/shop.Domain/User.cs
@@ -0,0 +1,124 @@
+﻿namespace shop.Domain;
+using System;
+using System.Net.Mail;
+
+public class User
+{
+    private string _mail;
+    private string _address;
+    private string _password;
+
+    public User()
+    {
+        _mail = string.Empty;
+        _address = string.Empty;
+        _password = string.Empty;
+    }
+
+    public int Id { get; set; }
+
+    public string Mail
+    {
+        get => _mail;
+        set
+        {
+            ValidateMail(value);
+            _mail = value;
+        }
+    }
+
+    public string Address
+    {
+        get => _address;
+        set
+        {
+            ValidateRequiredField(value, nameof(Address));
+            _address = value;
+        }
+    }
+
+    public string Password
+    {
+        get => _password;
+        set
+        {
+            ValidateRequiredField(value, nameof(Password));
+            _password = value;
+        }
+    }
+
+    public DateTime? DeletionDate { get; set; }
+
+    public void Delete()
+    {
+        DeletionDate = DateTime.Now;
+    }
+
+    private void ValidateMail(string mail)
+    {
+        if (string.IsNullOrEmpty(mail))
+        {
+            throw new RequiredValueIsEmptyException("User mail cannot be empty.");
+        }
+        if (!IsValidEmail(mail))
+        {
+            throw new InvalidValueException("User mail is not valid.");
+        }
+    }
+
+    private void ValidateRequiredField(string value, string fieldName)
+    {
+        if (string.IsNullOrEmpty(value))
+        {
+            throw new RequiredValueIsEmptyException($"User {fieldName} cannot be empty.");
+        }
+    }
+
+    private bool IsValidEmail(string email)
+    {
+        try
+        {
+            var mailAddress = new MailAddress(email);
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is not User other) return false;
+
+        return Id == other.Id &&
+               Mail == other.Mail &&
+               Address == other.Address &&
+               Password == other.Password;
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Id, Mail, Address, Password);
+    }
+}
+
+[Serializable]
+internal class InvalidValueException : Exception
+{
+    public InvalidValueException() { }
+
+    public InvalidValueException(string? message) : base(message) { }
+
+    public InvalidValueException(string? message, Exception? innerException) : base(message, innerException) { }
+}
+
+[Serializable]
+internal class RequiredValueIsEmptyException : Exception
+{
+    public RequiredValueIsEmptyException() { }
+
+    public RequiredValueIsEmptyException(string? message) : base(message) { }
+
+    public RequiredValueIsEmptyException(string? message, Exception? innerException) : base(message, innerException) { }
+}
