d781cef6aa23bcf91965e1ae9b661d00902a0d04
Max Halty <halty.maximo@principal.com>
2024-09-17 20:41:59 -0300

    Added a test for ExceptionHandler

---
+++ filters-Webapi-Shop/shop.WebApi.UnitTests/FiltersServerTest.cs
@@ -0,0 +1,66 @@
+using FluentAssertions;
+using System.Text;
+using shop.WebApi.Controllers;
+using shop.WebApi.Services;
+using shop.WebApi.Common;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace shop.WebApi.UnitTests
+{
+    [TestClass]
+    public sealed class FiltersServerTest
+    {
+        private TestServer _server;
+        private HttpClient _client;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            var builder = new WebHostBuilder()
+                .ConfigureServices(services =>
+                {
+                    services.AddControllers()
+                        .AddApplicationPart(typeof(ProductsController).Assembly);
+
+                    services.AddSingleton<IProductService, ProductService>();
+
+                    services.AddExceptionHandler<CustomExceptionHandler>();
+                    services.AddProblemDetails();
+                })
+                .Configure(app =>
+                {
+                    app.UseExceptionHandler();
+                    app.UseRouting();
+                    app.UseEndpoints(endpoints =>
+                    {
+                        endpoints.MapControllers();
+                    });
+                });
+            
+            _server = new TestServer(builder);
+            _client = _server.CreateClient();
+        }
+
+        #region Create
+        
+        #region Handled Error
+        [TestMethod]
+        public void Create_WhenProductIsNull_ShouldThrowException()
+        {
+            // Arrange
+            StringContent content = new StringContent("{}", Encoding.UTF8, "application/json");
+
+            // Act
+            var response = _client.PostAsync("products", content).Result;
+
+            // Assert
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.InternalServerError);
+        }
+        #endregion
+        
+        #endregion
+    }
+}
