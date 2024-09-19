d7c230d7647c8236b9f37c56b4a3a27eacc69013
Max Halty <halty.maximo@principal.com>
2024-09-18 23:08:12 -0300

    unit test for filter

---
+++ filters-Webapi-Shop/shop.WebApi.UnitTests/UnitFiltersServerTest.cs
@@ -0,0 +1,46 @@
+using shop.WebApi.Common;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace shop.WebApi.UnitTests
+{
+    [TestClass]
+    public sealed class UnitFiltersServerTest
+    {
+        #region Create
+
+        #region Handled Error
+        [TestMethod]
+        public void OnException_InvokeException_ReturnsBadRequest()
+        {
+            // Arrange 
+            var _logger = new Mock<ILogger<CustomExceptionFilter>>();;
+
+            var customExceptionFilter = new CustomExceptionFilter(_logger.Object);
+
+            var httpContext = new DefaultHttpContext();
+
+            var actionContext = new ActionContext(httpContext, new(), new(), new());
+            var exceptionContext = new ExceptionContext(actionContext, new List<IFilterMetadata>())
+            {
+                Exception = new ArgumentNullException("The name can not be null or empty")
+            };
+
+            // Act
+            customExceptionFilter.OnException(exceptionContext);
+
+            // Assert
+            Assert.IsInstanceOfType<ObjectResult>(exceptionContext.Result);
+            var exception = exceptionContext.Result as ObjectResult;
+            Assert.IsInstanceOfType<ProblemDetails>(exception!.Value);
+            var message = exception.Value as ProblemDetails;
+            Assert.AreEqual(400, message!.Status);
+        }
+        #endregion
+        
+        #endregion
+    }
+}
